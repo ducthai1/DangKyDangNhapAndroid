5791b998aeb7326fbd00ed86a996a4dab71c72b2
Thái Tăng Đức <thaitangduc1@gmail.com>
2023-04-27 01:03:49 +0700

    some

--- app/src/main/java/com/example/dangkydangnhap/customer/home/HomeActivity.java
+++ app/src/main/java/com/example/dangkydangnhap/customer/home/HomeActivity.java
@@ -29,31 +29,31 @@ public class HomeActivity extends AppCompatActivity {
 
 //        btnBottom_Nav_HomeID.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
 //            @Override
-////            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-////                switch (item.getItemId()){
-////                    case R.id.nav_home:
-////                        setFragment(new HomeFragment());
-////                        return true;
-////                    case R.id.nav_my_ords:
-////                        setFragment(new MyOrderFragment());
-////                        return true;
-////                    case R.id.nav_likes:
-////                        setFragment(new CustomerLikeFragment());
-////                        return true;
-////                    case R.id.nav_me:
-////                        setFragment(new CustomerMeFragment());
-////                        return true;
-////                    case R.id.nav_notifications:
-////                        setFragment(new CustomerNotificationFragment());
-////                        return true;
-////                }
-////                return false;
-////            }
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch (item.getItemId()){
+//                    case R.id.nav_home:
+//                        setFragment(new HomeFragment());
+//                        return true;
+//                    case R.id.nav_my_ords:
+//                        setFragment(new MyOrderFragment());
+//                        return true;
+//                    case R.id.nav_likes:
+//                        setFragment(new CustomerLikeFragment());
+//                        return true;
+//                    case R.id.nav_me:
+//                        setFragment(new CustomerMeFragment());
+//                        return true;
+//                    case R.id.nav_notifications:
+//                        setFragment(new CustomerNotificationFragment());
+//                        return true;
+//                }
+//                return false;
+//            }
 //        });
     }
-//    private void setFragment(Fragment fragment){
-//        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-//        fragmentTransaction.replace(R.id.home_framelayout, fragment);
-//        fragmentTransaction.commit();
-//    }
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fl_home, fragment);
+        fragmentTransaction.commit();
+    }
 }
